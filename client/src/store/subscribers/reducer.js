45a6c48b7a8bdcdc49c05166bae5ac61c1ba7ecc
Valeria Vitvytska <leranmu@gmail.com>
2022-10-25 09:52:47 +0300

    feat:step4 withDB

---
+++ client/src/store/subscribers/reducer.js
@@ -0,0 +1,59 @@
+import { addSubscriber,setSubscribers } from "./actions";
+
+const initialValue = {
+    value:[],
+    isLoading:true
+ 
+}
+const subscriberReducer =(state =initialValue,action) =>{
+
+    switch (action.type){
+case setSubscribers:{
+
+    return {value:action.payload,isLoading:false}
+}
+
+       
+        case addSubscriber: {
+                let users = action.payload.users
+                let index;
+
+             if(action.payload.index === 0){
+index = 1
+             }else{
+                 index = 0
+             }
+             let subscribers = users[action.payload.index].subscribers
+ 
+
+             subscribers.push(users[index])
+             users[action.payload.index].subscribers = subscribers
+ 
+        
+         
+
+             fetch('http://localhost:3001/users', {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({subscriber:users[index] , userIndex:action.payload.index})
+})
+if(subscribers.length < 50){
+    localStorage.setItem('users',JSON.stringify(users))
+
+return {value:JSON.parse(localStorage.getItem('users')),isLoading:false} }
+else if (subscribers.length >= 50){
+   return {value:users,isLoading:false}
+}
+            }
+           
+        default:{
+            return state
+        }
+        
+    }
+
+
+}
+export default subscriberReducer
